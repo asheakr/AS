ad3eda02ef5ac58e4b01ace7d7d5226c0d3a1382
Ahmed Abuakr <asheakr@pm.me>
2024-11-05 23:13:42 +0000

    f

---
+++ build/CMakeFiles/frames_publisher_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/frames_publisher_node.dir/src/frames_publisher_node.cpp.o"
+  "devel/lib/two_drones_pkg/frames_publisher_node"
+  "devel/lib/two_drones_pkg/frames_publisher_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/frames_publisher_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
